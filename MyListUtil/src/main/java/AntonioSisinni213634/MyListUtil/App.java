7f445d174295baff16ee0ec70ee9d12996126108
ANSIoso <92176296+ANSIoso@users.noreply.github.com>
2021-10-28 09:09:22 +0200

    first commit

---
+++ MyListUtil/src/main/java/AntonioSisinni213634/MyListUtil/App.java
@@ -0,0 +1,13 @@
+package AntonioSisinni213634.MyListUtil;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
